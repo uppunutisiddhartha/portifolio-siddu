096ef0b5aee5a544c1e9fef9916859923d07cf24
uppunutisiddhartha <you@example.com>
2025-04-12 14:09:36 +0530

    static collect

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
