8eb6ab4e6c25f1e44c92c0d2fb73f820c6ac35f5
uppunutisiddhartha <you@example.com>
2025-04-13 20:10:02 +0530

    siddu updated

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'uppunutisiddhartha@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'xsux yxco xngq bcsu'  # Replace with your email password
+EMAIL_HOST_USER = 'uppunutisiddhartha@gmail.com'  
+EMAIL_HOST_PASSWORD = 'xsux yxco xngq bcsu' 
